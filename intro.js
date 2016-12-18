a6a36a4d091465bd20e70c1fbd824659ce35298c
nckng <nng3@stuy.edu>
2016-12-17 22:45:46 -0500

    done

--- intro.js
+++ intro.js
@@ -1,6 +1,23 @@
 var l = document.getElementById("thelist");
 var b = document.getElementById("b");
 var a = document.getElementsByTagName("LI");
+var x = document.getElementById('h').innerHTML;
+var v = document.getElementById('button');
+var list = document.getElementById('list');
+
+for (var ctr=0; ctr < a.length; ctr++){	
+    a[ctr].addEventListener('click',function(){
+	this.remove()
+    });
+    a[ctr].addEventListener('mouseover',function(){
+	var h = document.getElementById('h');
+	h.innerHTML = this.innerHTML;
+    });
+    a[ctr].addEventListener('mouseout',function(){
+	var h = document.getElementById('h');
+	h.innerHTML = x;
+    });
+}
 
 b.addEventListener('click',function(){
     console.log("hello");
@@ -8,16 +25,37 @@ b.addEventListener('click',function(){
     s.innerHTML = "NEW ITEM!"
     l.appendChild(s);
     console.log(a);
-    a = document.getElementsByTagName("LI");
+    a = document.getElementsByTagName("LI")
+    for (var ctr=0; ctr < a.length; ctr++){	
+	a[ctr].addEventListener('click',function(){
+	    this.remove();
+	});
+	a[ctr].addEventListener('mouseover',function(){
+	    var h = document.getElementById('h');
+	    h.innerHTML = this.innerHTML;
+	});
+	
+	a[ctr].addEventListener('mouseout',function(){
+	    var h = document.getElementById('h');
+	    h.innerHTML = x;
+	});
+    }
 });
 
-for (var ctr=0; ctr < a.length; ctr++){
+var one = 1;
+var two = 1;
 
-    a[ctr].addEventListener('click',function(){
-	console.log(a.length);
-    });
-    
-}
+
+v.addEventListener('click',function(){
+    var r = document.createElement("LI");
+    r.innerHTML = one;
+    list.appendChild(r);
+    var stor = one + two;
+    one = two;
+    two = stor;
+});
+		   
+
 
 
 
